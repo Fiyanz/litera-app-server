cc2e72f8eb9d6ccd9144ade5f7658948acbc4d9e
Fiyanz <dctbagalyus@gmail.com>
2025-07-02 11:09:00 +0700

    remove customer

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from src.controllers.customer_controller import route as customer_router
 from src.controllers.user_controller import route as user_router
 from src.db.database import create_tabels
 
 app = FastAPI()
 
-app.include_router(customer_router, prefix="/api", tags=["Customers"])
 app.include_router(user_router, prefix="/api", tags=["Users"])
 app.include_router(create_tabels, prefix="/api", tags=["Auth"])
 
